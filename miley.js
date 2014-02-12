12724253c967360f1d9a3c044022d5c85d534a3f
Cecilia Bodin <cecilia.bodin@gmail.com>
2014-02-12 18:15:04 -0500

    twitter feed now works

--- miley.js
+++ miley.js
@@ -6,11 +6,31 @@
 // This code will be executed when the page finishes loading
 window.addEventListener('load', function(){
     // Fill me in!
+
 }, false);
 
 // Add more supporting code here!
 
 document.addEventListener('swagreceived', function(event){
-	var usrname = event.handle;
-	var test = event.text;
+	var username = event.handle;
+	var text = event.text;
+
+		// we have an unordered list of Tweets
+	var ul = document.getElementById('tweets');
+	    
+	// create a new li element for the Tweet, and append it
+	var li = document.createElement('li');
+	li.innerHTML = '<strong>' + event.handle + '</strong> ' + event.text;
+	ul.insertBefore(li);
+
+
+	// if (ul.length > 25) {
+	// 	delete last li child
+	// }
+
 })
+
+
+
+var elem = document.getElementById('data');
+  elem.scrollTop = elem.scrollHeight;
