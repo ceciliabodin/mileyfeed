24d04229e0fa7670d7cd6f52673895f91ab6d72b
Cecilia Bodin <cecilia.bodin@gmail.com>
2014-02-13 11:24:55 -0500

    added the insertBefore method

--- miley.js
+++ miley.js
@@ -14,21 +14,43 @@ window.addEventListener('load', function(){
 document.addEventListener('swagreceived', function(event){
 	var username = event.handle;
 	var text = event.text;
+	// var pic event.profile_image_url;
 
 		// we have an unordered list of Tweets
 	var ul = document.getElementById('tweets');
 	    
 	// create a new li element for the Tweet, and append it
+
 	var li = document.createElement('li');
+
 	li.innerHTML = '<strong>' + event.handle + '</strong> ' + event.text;
-	ul.insertBefore(li);
+
+	ul.insertBefore(li, ul.firstChild);
+
+	if (ul.length > 25) {
+		ul.pop();
+	};	
 
 
-	// if (ul.length > 25) {
-	// 	delete last li child
-	// }
+// var node=document.getElementById("myList2").lastChild;
+// var list=document.getElementById("myList1");
+// list.insertBefore(node,list.childNodes[0]);
 
-})
+	// $('ul').onLoad(function(){
+
+	// window.scroll(0,300);
+
+	// });
+
+	// $(document).ready(function(){
+ //    $('#tweet_container').scrollTop($('ul li').last().position().top + $('ul li').last().height());
+	// });
+
+
+	// var list = document.getElementById('ul');
+ //  	list.scrollBottom = list.scrollHeight;
+
+});
 
 
 
